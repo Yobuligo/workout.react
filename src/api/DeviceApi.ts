92f1cd3a91a4314f856a5276aabee88ff11c9065
Yobuligo <hoffmannpeter82@gmail.com>
2024-02-05 16:39:50 +0100

    Correct http-method in DeviceApi

--- src/api/DeviceApi.ts
+++ src/api/DeviceApi.ts
@@ -3,7 +3,7 @@ import { IDevice } from "../shared/model/exercise/IDevice";
 export class DeviceApi {
   findAll(): Promise<IDevice[]> {
     return new Promise(async (resolve) => {
-      const response = await fetch("/devices");
+      const response = await fetch("/devices", { method: "GET" });
       const devices = await response.json();
       resolve(devices);
     });
--- src/pages/WorkoutConfigPage.tsx
+++ src/pages/WorkoutConfigPage.tsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { DeviceApi } from "../api/DeviceApi";
 import { Page } from "../components/pages/Page";
 import { DeviceSelectorList } from "../features/deviceSelectorList/DeviceSelectorList";
-import { IDevice } from "../shared/model/exercise/IDevice";
-import { request } from "../utils/request";
 import useTranslation from "../hooks/useTranslation";
 import { texts } from "../i18n/texts";
+import { IDevice } from "../shared/model/exercise/IDevice";
+import { request } from "../utils/request";
 
 export const WorkoutConfigPage: React.FC = () => {
   const [devices, setDevices] = useState<IDevice[]>([]);
@@ -32,7 +32,10 @@ export const WorkoutConfigPage: React.FC = () => {
     });
 
   return (
-    <Page subTitle={t(texts.workoutConfigPage.title)} title={"Workout App"}>
+    <Page
+      subTitle={t(texts.workoutConfigPage.title)}
+      title={t(texts.workoutConfigPage.subTitle)}
+    >
       <DeviceSelectorList
         devices={devices}
         onSelect={onSelectDevice}
