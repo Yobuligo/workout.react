bc57cac36d75767c92ae98010188dfded791b1fd
Yobuligo <hoffmannpeter82@gmail.com>
2024-02-13 14:00:33 +0100

    Add Difficulty and MuscleGroup

--- src/shared/model/exercise/IExercise.ts
+++ src/shared/model/exercise/IExercise.ts
@@ -1,8 +1,12 @@
-import { IDevice } from "./IDevice";
+import { Difficulty } from "../../types/Difficulty";
+import { MuscleGroup } from "../../types/MuscleGroup";
 import { IEntity } from "../core/IEntity";
 import { IHaveDescription } from "../core/IHaveDescription";
 import { IHaveName } from "../core/IHaveName";
+import { IDevice } from "./IDevice";
 
 export interface IExercise extends IEntity, IHaveName, IHaveDescription {
   devices: IDevice[];
+  difficulty: Difficulty;
+  muscleGroups: MuscleGroup[];
 }
---
+++ src/shared/types/MuscleGroup.ts
@@ -0,0 +1,8 @@
+export enum MuscleGroup {
+  ARMS = "Arms",
+  BACK = "Back",
+  CHEST = "Chest",
+  CORE = "Core",
+  LEGS = "Legs",
+  SHOULDER = "Shoulder",
+}
