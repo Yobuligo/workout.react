09479b8a57781e97704ef399126286099f820f54
Yobuligo <hoffmannpeter82@gmail.com>
2024-02-19 12:16:36 +0100

    Add ExerciseTypeSelector style

--- src/features/exerciseTypeSelector/ExerciseTypeSelector.tsx
+++ src/features/exerciseTypeSelector/ExerciseTypeSelector.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Api } from "../../api/Api";
 import { AsyncLoad } from "../../components/asyncLoad/AsyncLoad";
 import { Card } from "../../components/card/Card";
+import useTranslation from "../../hooks/useTranslation";
+import { texts } from "../../i18n/texts";
 import { IExerciseType } from "../../shared/model/exercise/IExerciseType";
 import styles from "./ExerciseTypeSelector.module.scss";
 import { IExerciseTypeSelectorProps } from "./IExerciseTypeSelectorProps";
@@ -9,6 +11,7 @@ import { IExerciseTypeSelectorProps } from "./IExerciseTypeSelectorProps";
 export const ExerciseTypeSelector: React.FC<IExerciseTypeSelectorProps> = (
   props
 ) => {
+  const { t } = useTranslation();
   const [exerciseTypes, setExerciseTypes] = useState<IExerciseType[]>([]);
 
   const items = exerciseTypes.map((exerciseType) => (
@@ -29,6 +32,7 @@ export const ExerciseTypeSelector: React.FC<IExerciseTypeSelectorProps> = (
         setExerciseTypes(exerciseTypes);
       }}
     >
+      <p>{t(texts.exerciseTypeSelector.explanation)}</p>
       <div className={styles.exerciseTypeSelector}>{items}</div>
     </AsyncLoad>
   );
