a1e982699ebad2b7315741ee5e3ca567af16af35
Yobuligo <hoffmannpeter82@gmail.com>
2024-02-28 21:02:19 +0100

    Configure Schemes

--- src/shared/model/workout/workoutScheme/WorkoutSchemes.ts
+++ src/shared/model/workout/workoutScheme/WorkoutSchemes.ts
@@ -1,12 +1,8 @@
-import { Constructor } from "../../../../core/Constructor";
-import { Scheme } from "../../core/scheme/Scheme";
 import { configureSchemes } from "../../core/scheme/configureSchemes";
 import { Conditioning } from "../workoutBlock/Conditioning";
 import { Power } from "../workoutBlock/Power";
 import { Stretching } from "../workoutBlock/Stretching";
-import { WorkoutBlock } from "../workoutBlock/WorkoutBlock";
-
-type WorkoutScheme = Scheme<Scheme<Constructor<WorkoutBlock>>>;
+import { WorkoutScheme } from "./WorkoutScheme";
 
 export const WorkoutSchemes: WorkoutScheme = configureSchemes({
   default: {
