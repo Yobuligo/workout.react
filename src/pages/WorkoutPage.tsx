a6c4da4fc899bf9cb221267704ea3574edb9e401
Yobuligo <hoffmannpeter82@gmail.com>
2024-03-03 22:56:25 +0100

    Add navigation back from Workout to WorkoutOverview

--- src/pages/WorkoutPage.tsx
+++ src/pages/WorkoutPage.tsx
@@ -1,12 +1,19 @@
 import { Page } from "../components/pages/Page";
+import { useRouteParam } from "../hooks/useRouteParam";
 import useTranslation from "../hooks/useTranslation";
 import { texts } from "../i18n/texts";
+import { Routes } from "../routes/Routes";
+import { WorkoutType } from "../shared/types/WorkoutType";
 
 export const WorkoutPage: React.FC = () => {
   const { t } = useTranslation();
+  const workoutType = useRouteParam<WorkoutType>("workout-type");
 
   return (
     <Page
+      navigateBackPath={Routes.workoutOverview.toPath({
+        "workout-type": workoutType,
+      })}
       title={t(texts.workoutPage.title)}
       subTitle={t(texts.workoutPage.subtitle)}
     >
