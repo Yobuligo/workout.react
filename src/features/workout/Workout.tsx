1f6fbe384f66b03ccafab014db0004ea949b11cf
Yobuligo <hoffmannpeter82@gmail.com>
2024-03-05 21:17:47 +0100

    Display WorkoutBlock and WorkoutExercise

--- src/features/workout/Workout.tsx
+++ src/features/workout/Workout.tsx
@@ -1,10 +1,40 @@
+import { useMemo, useState } from "react";
+import useTranslation from "../../hooks/useTranslation";
+import { IWorkoutIterator } from "../../shared/model/workout/workoutIterator/IWorkoutIterator";
+import { WorkoutIterator } from "../../shared/model/workout/workoutIterator/WorkoutIterator";
+import { IWorkoutStep } from "../../shared/model/workout/workoutStep/IWorkoutStep";
 import { IWorkoutProps } from "./IWorkoutProps";
 
 export const Workout: React.FC<IWorkoutProps> = (props) => {
+  const { t } = useTranslation();
+  const workoutIterator: IWorkoutIterator = useMemo(
+    () => new WorkoutIterator(props.workout),
+    [props.workout]
+  );
+
+  const [workoutStep, setWorkoutStep] = useState<IWorkoutStep | undefined>(
+    undefined
+  );
+
   return (
     <>
-      <div>Header</div>
-      <div>Exercise</div>
+      <button
+        onClick={() => {
+          if (workoutIterator.hasNext) {
+            const workoutStep = workoutIterator.next();
+            setWorkoutStep(workoutStep);
+          }
+        }}
+      >
+        next
+      </button>
+      <div>
+        {workoutStep?.workoutBlock.title && t(workoutStep.workoutBlock.title)}
+      </div>
+      <div>
+        {workoutStep?.workoutExercise.title &&
+          t(workoutStep.workoutExercise.title)}
+      </div>
     </>
   );
 };
