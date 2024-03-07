9697dfaae7943e7f6abfa48c6bb3eb3843487651
Yobuligo <hoffmannpeter82@gmail.com>
2024-03-07 07:26:30 +0100

    Refactor WorkoutIterator

--- src/shared/services/workoutIterator/WorkoutIterator.ts
+++ src/shared/services/workoutIterator/WorkoutIterator.ts
@@ -1,83 +1,31 @@
 import { IllegalStateError } from "../../../error/IllegalStateError";
-import { error } from "../../utils/error";
 import { IWorkout } from "../../model/workout/workout/IWorkout";
-import { IWorkoutBlock } from "../../model/workout/workoutBlock/IWorkoutBlock";
-import { IWorkoutBlockItem } from "../../model/workout/workoutBlock/IWorkoutBlockItem";
-import { IWorkoutExercise } from "../../model/workout/workoutExercise/IWorkoutExercise";
-import { isWorkoutExercise } from "../../model/workout/workoutExercise/isWorkoutExercise";
 import { IWorkoutStep } from "../../model/workout/workoutStep/IWorkoutStep";
+import { WorkoutFlatter } from "./../workoutFlatter/WorkoutFlatter";
 import { IWorkoutIterator } from "./IWorkoutIterator";
 
 export class WorkoutIterator implements IWorkoutIterator {
-  private workoutBlockCursor = 0;
-  private workoutExerciseCursor = 0;
+  private cursor = 0;
+  private readonly workoutSteps: IWorkoutStep[];
 
-  constructor(private readonly workout: IWorkout) {}
+  constructor(workout: IWorkout) {
+    const workoutFlatter = new WorkoutFlatter(workout);
+    this.workoutSteps = workoutFlatter.toFlat();
+  }
 
   get hasNext(): boolean {
-    return (
-      this.getNextFromCurrentBlock() !== undefined ||
-      this.getNextFromNextBlock() !== undefined
-    );
+    return this.workoutSteps[this.cursor] !== undefined;
   }
 
   next(): IWorkoutStep {
-    let workoutExercise = this.getNextFromCurrentBlock();
-    if (workoutExercise) {
-      const workoutBlock = this.getWorkoutBlock();
-      this.workoutExerciseCursor++;
-      return {
-        workoutBlock,
-        workoutExercise,
-      };
+    const workoutStep = this.workoutSteps[this.cursor];
+    if (!workoutStep) {
+      throw new IllegalStateError(
+        `Error while getting next workout exercise. No next exercise workout exercise found. Use hasNext to check if another workout exercise is available.`
+      );
     }
 
-    workoutExercise = this.getNextFromNextBlock();
-    if (workoutExercise) {
-      this.workoutBlockCursor++;
-      this.workoutExerciseCursor = 1;
-      return {
-        workoutBlock: this.getWorkoutBlock(),
-        workoutExercise: workoutExercise,
-      };
-    }
-
-    throw new IllegalStateError(
-      `Error while getting next workout exercise. No next exercise workout exercise found. Use hasNext to check if another workout exercise is available.`
-    );
-  }
-
-  private getNextFromCurrentBlock(): IWorkoutExercise | undefined {
-    const workoutBlock = this.getWorkoutBlock();
-    const workoutBlockItem = workoutBlock.items[this.workoutExerciseCursor];
-    return this.extractWorkoutExercise(workoutBlockItem);
-  }
-
-  private getNextFromNextBlock(): IWorkoutExercise | undefined {
-    const workoutBlock = this.workout.blocks[this.workoutBlockCursor + 1];
-    if (!workoutBlock) {
-      return undefined;
-    }
-
-    const workoutBlockItem = workoutBlock.items[0];
-    return this.extractWorkoutExercise(workoutBlockItem);
-  }
-
-  private getWorkoutBlock(): IWorkoutBlock {
-    return (
-      this.workout.blocks[this.workoutBlockCursor] ??
-      error(
-        `Error while getting next workout block. Workout block not found. Workout block at position '${this.workoutBlockCursor}' must be available.`
-      )
-    );
-  }
-
-  private extractWorkoutExercise(
-    workoutBlockItem: IWorkoutBlockItem
-  ): IWorkoutExercise | undefined {
-    if (workoutBlockItem && !isWorkoutExercise(workoutBlockItem)) {
-      throw new IllegalStateError(`Error while getting next workout Exercise.`);
-    }
-    return workoutBlockItem;
+    this.cursor++;
+    return workoutStep;
   }
 }
