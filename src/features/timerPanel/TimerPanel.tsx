94e092fdc4fe574f767f10c8951c2daf7b31db1e
Yobuligo <hoffmannpeter82@gmail.com>
2024-03-19 20:30:21 +0100

    Add icons for pause, play and reset

--- src/features/timerPanel/TimerPanel.tsx
+++ src/features/timerPanel/TimerPanel.tsx
@@ -1,3 +1,4 @@
+import { useRenderSeconds } from "../../hooks/useRenderSeconds";
 import { useTimer } from "../../hooks/useTimer";
 import useTranslation from "../../hooks/useTranslation";
 import { texts } from "../../i18n/texts";
@@ -7,35 +8,51 @@ import styles from "./TimerPanel.module.scss";
 
 export const TimerPanel: React.FC<ITimerPanelProps> = (props) => {
   const { t } = useTranslation();
+  const renderSeconds = useRenderSeconds();
   const timer = useTimer(props.seconds);
 
   const initialContent = (
     <div className={styles.buttons}>
-      <TimerPanelButton>{t(texts.timerPanel.start)}</TimerPanelButton>
+      <TimerPanelButton onClick={timer.start}>
+        {t(texts.timerPanel.start)}
+      </TimerPanelButton>
     </div>
   );
 
   const runningContent = (
     <div className={styles.buttons}>
-      <TimerPanelButton>{t(texts.timerPanel.reset)}</TimerPanelButton>
-      <TimerPanelButton>{t(texts.timerPanel.pause)}</TimerPanelButton>
+      <TimerPanelButton onClick={timer.reset}>
+        {t(texts.timerPanel.reset)}
+      </TimerPanelButton>
+      <TimerPanelButton
+        onClick={() => {
+          timer.stop();
+        }}
+      >
+        {t(texts.timerPanel.pause)}
+      </TimerPanelButton>
     </div>
   );
 
   const pauseContent = (
     <div className={styles.buttons}>
-      <TimerPanelButton>{t(texts.timerPanel.reset)}</TimerPanelButton>
-      <TimerPanelButton>{t(texts.timerPanel.continue)}</TimerPanelButton>
+      <TimerPanelButton onClick={timer.reset}>
+        {t(texts.timerPanel.reset)}
+      </TimerPanelButton>
+      <TimerPanelButton onClick={timer.start}>
+        {t(texts.timerPanel.continue)}
+      </TimerPanelButton>
     </div>
   );
 
   return (
-    <>
+    <div className={styles.timerPanel}>
+      {renderSeconds(timer.remainingSeconds)}
       {!timer.isStarted
         ? initialContent
         : timer.isRunning
         ? runningContent
         : pauseContent}
-    </>
+    </div>
   );
 };
