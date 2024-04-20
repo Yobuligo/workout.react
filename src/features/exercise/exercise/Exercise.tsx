9bffa66157ef6b75f8f9fd516d46ff91f32c3cb2
Yobuligo <hoffmannpeter82@gmail.com>
2024-04-20 18:18:41 +0200

    Style exercise

--- src/features/exercise/exercise/Exercise.tsx
+++ src/features/exercise/exercise/Exercise.tsx
@@ -3,8 +3,9 @@ import { texts } from "../../../i18n/texts";
 import { IExerciseProps } from "./IExerciseProps";
 import { ReactComponent as UpArrow } from "../../../assets/icons/up-arrow.svg";
 import { ReactComponent as DownArrow } from "../../../assets/icons/down-arrow.svg";
-import styles from "./Exericse.module.scss";
+import styles from "./Exercise.module.scss";
 import { useToggle } from "../../../hooks/useToggle";
+import { Difficulty } from "../../../shared/types/Difficulty";
 
 export const Exercise: React.FC<IExerciseProps> = (props) => {
   const [expand, toggleExpand] = useToggle(false);
@@ -12,24 +13,33 @@ export const Exercise: React.FC<IExerciseProps> = (props) => {
 
   const devices = props.exercise.devices?.map((device) => device.title);
 
+  const muscleGroups = props.exercise.muscleGroups.join(",");
+
   return (
-    <div className={styles.exercise}>
-      <div>{t(props.exercise.title)}</div>
-      {expand === false ? (
-        <DownArrow width={"2rem"} onClick={() => toggleExpand()} />
-      ) : (
-        <UpArrow width={"2rem"} onClick={() => toggleExpand()} />
+    <>
+      <div className={styles.exercise}>
+        <div>{t(props.exercise.title)}</div>
+        {expand === false ? (
+          <DownArrow width={"2rem"} onClick={() => toggleExpand()} />
+        ) : (
+          <UpArrow width={"2rem"} onClick={() => toggleExpand()} />
+        )}
+      </div>
+      {expand === true && (
+        <>
+          <div>
+            {t(texts.exercise.description)}: {t(props.exercise.description)}
+          </div>
+          <div>
+            {t(texts.exercise.difficulty)}:{" "}
+            {Difficulty[props.exercise.difficulty]}
+          </div>
+          <div>
+            {t(texts.exercise.muscleGroup)}: {muscleGroups}
+          </div>
+          {devices && devices.length > 0 && <div>Devices: {devices}</div>}
+        </>
       )}
-      {/* <div>
-        {t(texts.exercise.description)}: {t(props.exercise.description)}
-      </div>
-      <div>
-        {t(texts.exercise.difficulty)}: {Difficulty[props.exercise.difficulty]}
-      </div>
-      <div>
-        {t(texts.exercise.muscleGroup)}: {muscleGroups}
-      </div>
-      {devices && devices.length > 0 && <div>Devices: {devices}</div>} */}
-    </div>
+    </>
   );
 };
--- src/features/exercise/exerciseList/ExerciseList.tsx
+++ src/features/exercise/exerciseList/ExerciseList.tsx
@@ -1,11 +1,15 @@
 import { Card } from "../../../components/card/Card";
+import { style } from "../../../core/style";
 import { Exercise } from "../exercise/Exercise";
 import styles from "./ExerciseList.module.scss";
 import { IExerciseListProps } from "./IExerciseListProps";
 
 export const ExerciseList: React.FC<IExerciseListProps> = (props) => {
   const items = props.exercises.map((exercise, index) => (
-    <div key={index} className={styles.item}>
+    <div
+      key={index}
+      className={style(styles.item, index > 0 ? styles.divider : "")}
+    >
       <Exercise exercise={exercise} />
     </div>
   ));
