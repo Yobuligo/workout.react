f30d0926c7d6a306d7f0b553fa8f9733a2bf66fb
Yobuligo <hoffmannpeter82@gmail.com>
2024-04-21 11:37:26 +0200

    Adjust Switch colors

--- src/features/deviceSelectorList/DeviceSelectorList.tsx
+++ src/features/deviceSelectorList/DeviceSelectorList.tsx
@@ -1,20 +1,32 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Api } from "../../api/Api";
 import { AsyncLoad } from "../../components/asyncLoad/AsyncLoad";
 import { Switch } from "../../components/switch/Switch";
 import { AppContext } from "../../context/AppContext";
+import { request } from "../../core/request";
 import useTranslation from "../../hooks/useTranslation";
 import { texts } from "../../i18n/texts";
 import { IDevice } from "../../shared/model/device/IDevice";
+import { IExercise } from "../../shared/model/exercise/IExercise";
+import { WorkoutType } from "../../shared/types/WorkoutType";
 import { DeviceSelector } from "../deviceSelector/DeviceSelector";
+import { ExerciseList } from "../exercise/exerciseList/ExerciseList";
 import styles from "./DeviceSelectorList.module.scss";
 import { IDevicePickerListProps } from "./IDeviceSelectorListProps";
-import { ExerciseList } from "../exercise/exerciseList/ExerciseList";
 
 export const DeviceSelectorList: React.FC<IDevicePickerListProps> = (props) => {
   const [devices, setDevices] = useState<IDevice[]>([]);
   const context = useContext(AppContext);
   const { t } = useTranslation();
+  const [checked, setChecked] = useState(false);
+  const [exercises, setExercises] = useState<IExercise[]>([]);
+
+  useEffect(() => {
+    request(async () => {
+      const exercises = await Api.exercise.findByWorkoutType(WorkoutType.POWER);
+      setExercises(exercises);
+    });
+  }, [checked]);
 
   const onSelectDevice = (device: IDevice) =>
     context.selectedDevices.append(device);
@@ -42,12 +54,14 @@ export const DeviceSelectorList: React.FC<IDevicePickerListProps> = (props) => {
     >
       <div className={styles.header}>
         <p>{t(texts.deviceSelectorList.explanation)}</p>
-        <Switch />
+        <Switch checked={checked} onChange={setChecked} />
       </div>
       <div className={styles.items}>{items}</div>
-      <div>
-
-      </div>
+      {checked && (
+        <div>
+          <ExerciseList exercises={exercises} />
+        </div>
+      )}
     </AsyncLoad>
   );
 };
