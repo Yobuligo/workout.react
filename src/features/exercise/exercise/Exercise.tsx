1f60bba5c48dcfeaad33f0f049ce3eaa4bcfceb3
Yobuligo <hoffmannpeter82@gmail.com>
2024-04-22 22:20:21 +0200

    Style exercise list

--- src/features/exercise/exercise/Exercise.tsx
+++ src/features/exercise/exercise/Exercise.tsx
@@ -1,11 +1,11 @@
+import { ReactComponent as DownArrow } from "../../../assets/icons/down-arrow.svg";
+import { ReactComponent as UpArrow } from "../../../assets/icons/up-arrow.svg";
+import { useToggle } from "../../../hooks/useToggle";
 import useTranslation from "../../../hooks/useTranslation";
 import { texts } from "../../../i18n/texts";
-import { IExerciseProps } from "./IExerciseProps";
-import { ReactComponent as UpArrow } from "../../../assets/icons/up-arrow.svg";
-import { ReactComponent as DownArrow } from "../../../assets/icons/down-arrow.svg";
-import styles from "./Exercise.module.scss";
-import { useToggle } from "../../../hooks/useToggle";
 import { Difficulty } from "../../../shared/types/Difficulty";
+import styles from "./Exercise.module.scss";
+import { IExerciseProps } from "./IExerciseProps";
 
 export const Exercise: React.FC<IExerciseProps> = (props) => {
   const [expand, toggleExpand] = useToggle(false);
@@ -26,19 +26,23 @@ export const Exercise: React.FC<IExerciseProps> = (props) => {
         )}
       </div>
       {expand === true && (
-        <>
-          <div>
-            {t(texts.exercise.description)}: {t(props.exercise.description)}
-          </div>
-          <div>
-            {t(texts.exercise.difficulty)}:{" "}
-            {Difficulty[props.exercise.difficulty]}
-          </div>
-          <div>
-            {t(texts.exercise.muscleGroup)}: {muscleGroups}
-          </div>
-          {devices && devices.length > 0 && <div>Devices: {devices}</div>}
-        </>
+        <div className={styles.details}>
+          <div>{t(texts.exercise.description)}:</div>
+          <div>{t(props.exercise.description)}</div>
+
+          <div>{t(texts.exercise.difficulty)}:</div>
+          <div>{Difficulty[props.exercise.difficulty]}</div>
+
+          <div>{t(texts.exercise.muscleGroup)}:</div>
+          <div>{muscleGroups}</div>
+
+          {devices && devices.length > 0 && (
+            <>
+              <div>Devices:</div>
+              <div> {devices}</div>
+            </>
+          )}
+        </div>
       )}
     </>
   );
